ebd1d22b2c0a4715a25ec3c13033d180c8675978
Jake Yu <dev@eightydigital.com>
2014-10-22 14:07:57 +0800

    last edits: 10/22/2014 | 2:07PM

--- js/script.js
+++ js/script.js
@@ -26,21 +26,37 @@
 
      this.custom_specs = {
       'step2': {'fheight': 0, 
-                      'flength': 0 }
+                'flength': 0 },
+      'step3': {'material': ''}
      };
+     this.material = ['wood','metal', 'plastic', 'random', 'cardboard'];
 
      this.set_custom_specs_height = function(furniture_height){
         this.custom_specs['step2']['fheight'] = furniture_height;
      };
+          this.get_custom_specs_height = function(){
+              return this.custom_specs['step2']['fheight'];
+          };
      this.set_custom_specs_length = function(furniture_length){
         this.custom_specs['step2']['flength'] = furniture_length;
      };
+          this.get_custom_specs_length = function(){
+              return this.custom_specs['step2']['flength'];
+          };
+     //CHECK STEPS
      this.check_step2 = function(){
         if ( this.custom_specs['step2']['fheight'] > 0 && this.custom_specs['step2']['flength'] > 0){
           return true;
         }else{ 
           return false;
         }
+     };
+     this.check_step3 = function(){
+      console.log(this.custom_specs['step3']['material']);
+      if ( this.custom_specs['step3']['material'] )
+          return true;
+      else
+          return false;
      };
 
     this.get_custom_specs = function(){
